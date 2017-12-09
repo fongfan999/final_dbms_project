ecc7d3d24e027699f7b017fa2574f8051fa43750
The Tien <tiendt2311@gmail.com>
2017-12-09 12:39:17 +0700

    Finish crud ticket

---
+++ app/controllers/tickets_controller.rb
@@ -0,0 +1,49 @@
+class TicketsController < ApplicationController
+  before_action :set_ticket, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @tickets = Ticket.all
+  end
+
+  def show
+  end
+
+  def new
+    @ticket = Ticket.new
+  end
+
+  def edit
+  end
+
+  def create
+    @ticket = Ticket.new(ticket_params)
+
+    if @ticket.save
+      redirect_to @ticket, notice: 'Ticket was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @ticket.update(ticket_params)
+      redirect_to @ticket, notice: 'Ticket was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @ticket.destroy
+    redirect_to tickets_url, notice: 'Ticket was successfully destroyed.'
+  end
+
+  private
+    def set_ticket
+      @ticket = Ticket.find(params[:id])
+    end
+
+    def ticket_params
+      params.require(:ticket).permit(:flight, :start_date, :price, :quantity, :departure_id, :destination_id)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
+  resources :tickets
   root 'welcome#index'
 end
