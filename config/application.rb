71d31721cc6327963a30ab30052b5dd9bf0e8cf1
The Tien <tiendt2311@gmail.com>
2017-12-18 00:36:53 +0700

    added procfile

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,7 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+# Dotenv::Railtie.load
 
 module FinalDbmsProject
   class Application < Rails::Application
