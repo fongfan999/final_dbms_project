2e8b9ba41bd68584c2e5fa341b7eef83e8eba942
The Tien <tiendt2311@gmail.com>
2017-12-18 14:35:04 +0700

    addd lockable

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :confirmable,
-         :recoverable, :rememberable, :trackable, :validatable, :lockable
+         :recoverable, :rememberable, :trackable, :validatable
 end
