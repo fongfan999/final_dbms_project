eff73ce224a7a8b54fdcd4de2dd8fa23be609453
Phong Phan <fongfan999@gmail.com>
2017-12-18 12:50:41 +0700

    Remove dotenv gem

--- config/application.rb
+++ config/application.rb
@@ -6,8 +6,6 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-Dotenv::Railtie.load
-
 module FinalDbmsProject
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
