9ad1905cfd577444103f538896fc7c20cf65f8dc
The Tien <tiendt2311@gmail.com>
2018-01-08 10:21:39 +0700

    added user show

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,5 +1,21 @@
 class Admin::UsersController < ApplicationController
+  before_action :set_user, except: :index
   def index
     @users = User.all
   end
+
+  def show
+    @tickets = @user.tickets.page(params[:page])
+  end
+
+  def destroy
+    @user.destroy
+    flash[:notice] = "Destroyed!"
+    redirect_to admin_users_path
+  end
+
+  private
+    def set_user
+      @user = User.find(params[:id])
+    end
 end
